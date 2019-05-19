5c9ab5bee3b1aeb33cb82b0ab8ec765db3fd9055
Martin Labryga <martin@labryga.ch>
2019-05-19 14:44:55 +0200

    nginx local.conf for development configured

--- facelock/facelock/settings.py
+++ facelock/facelock/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJ_SECRETKEY")
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["facelock.ch"]
 
 
 # Application definition
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homepage',
 ]
 
 MIDDLEWARE = [
