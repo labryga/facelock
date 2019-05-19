34267ecc12b7921a65ea00ca3f82d4bd9fa0d418
Martin Labryga <martin@labryga.ch>
2019-05-19 15:09:42 +0200

    sass configured

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,1 +1,33 @@
-var gulp = require("gulp");
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('gulp-autoprefixer'),
+	concat = require('gulp-concat'),
+	babel = require('gulp-babel'),
+	uglifyjs = require('gulp-uglify'),
+	uglifycss = require('gulp-uglifycss'),
+	rename = require('gulp-rename');
+
+gulp.task('css', () => {
+	return gulp.src([
+		'../staticfiles/css/development/normalize.css',
+		'../staticfiles/css/development/sass/html.sass',
+		'../staticfiles/css/development/sass/header.sass',
+		'../staticfiles/css/development/sass/main.sass',
+		'../staticfiles/css/development/sass/footer.sass',
+
+	])
+			.pipe(sass().on('error', sass.logError))
+			.pipe(autoprefixer('last 5 versions', 'Android >= 2.3', 'ChromeAndroid >= 20'))
+			.pipe(concat('facelock.css'))
+			.pipe(gulp.dest('../staticfiles/css/production/'))
+			.pipe(uglifycss())
+			.pipe(rename('facelock.min.css'))
+		.pipe(gulp.dest('../staticfiles/css/production/'));
+});
+
+gulp.task('watch', () => {
+	gulp.watch('../staticfiles/css/development/sass/*', gulp.series('css'));
+});
+
