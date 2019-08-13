6784fc82364a6c3ce9bdb731e47cf15b29aae78f
Martin Labryga <martin@labryga.ch>
2019-08-13 21:16:24 +0200

    gulpfile indentation error fixed

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -11,44 +11,42 @@ var gulp = require('gulp'),
 	livereload = require('gulp-livereload');
 
 gulp.task('css', () => {
-	return gulp.src([
-		'../staticfiles/css/development/normalize.css',
-		'../staticfiles/css/development/sass/html.sass',
-		'../staticfiles/css/development/sass/header.sass',
-		'../staticfiles/css/development/sass/main.sass',
-		'../staticfiles/css/development/sass/footer.sass',
-
-	])
-			.pipe(sass().on('error', sass.logError))
-			.pipe(autoprefixer('last 5 versions', 'Android >= 2.3', 'ChromeAndroid >= 20'))
-			.pipe(concat('facelock.css'))
-			.pipe(gulp.dest('../staticfiles/production/'))
-			.pipe(uglifycss())
-			.pipe(rename('facelock.min.css'))
-		.pipe(gulp.dest('../staticfiles/production/'))
-			.pipe(livereload());
+  return gulp.src([
+	'../staticfiles/css/development/normalize.css',
+	'../staticfiles/css/development/sass/html.sass',
+	'../staticfiles/css/development/sass/header.sass',
+	'../staticfiles/css/development/sass/main.sass',
+	'../staticfiles/css/development/sass/footer.sass',
+  ])
+	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer('last 5 versions', 'Android >= 2.3', 'ChromeAndroid >= 20'))
+	.pipe(concat('facelock.css'))
+	.pipe(gulp.dest('../staticfiles/production/'))
+	.pipe(uglifycss())
+	.pipe(rename('facelock.min.css'))
+  .pipe(gulp.dest('../staticfiles/production/'))
+	.pipe(livereload());
 });
 
 gulp.task('javascript', () => {
-	return gulp.src('../staticfiles/js/development/modules/*.js')
-			.pipe(concat('facelock_es6_build.js'))
-			.pipe(gulp.dest('../staticfiles/js/development/'))
-			.pipe(babel({
-				presets: ['@babel/env'],
-				plugins: ['babel-plugin-loop-optimizer']
-			}))
-			.pipe(rename('facelock.js'))
-			.pipe(gulp.dest('../staticfiles/production/'))
-			.pipe(uglifyjs())
-			.pipe(rename('facelock.min.js'))
-		.pipe(gulp.dest('../staticfiles/production/'))
-			.pipe(livereload());
+  return gulp.src('../staticfiles/js/development/modules/*.js')
+	.pipe(concat('facelock_es6_build.js'))
+	.pipe(gulp.dest('../staticfiles/js/development/'))
+	.pipe(babel({
+		presets: ['@babel/env'],
+		plugins: ['babel-plugin-loop-optimizer']
+	}))
+	.pipe(rename('facelock.js'))
+	.pipe(gulp.dest('../staticfiles/production/'))
+	.pipe(uglifyjs())
+	.pipe(rename('facelock.min.js'))
+  .pipe(gulp.dest('../staticfiles/production/'))
+	.pipe(livereload());
 });
 
 gulp.task('watch', () => {
-	livereload.listen();
-	gulp.watch('../staticfiles/css/development/sass/*', gulp.series('css'));
-	gulp.watch('../staticfiles/js/development/modules/*.js', gulp.series('javascript'));
+  livereload.listen();
+  gulp.watch('../staticfiles/css/development/sass/*', gulp.series('css'));
+  gulp.watch('../staticfiles/js/development/modules/*.js', gulp.series('javascript'));
 });
-
 
